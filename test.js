e3228a243eb5b152cc72d38f750e2e108f2d0b07
Anshuman Bhartiya <anshuman.bhartiya@gmail.com>
2018-07-15 02:17:29 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+// The string:
+var str = "KydYBrNgHmyD#CZuioFH,Wj6VvEYowua?WEHZynsqCWTc#jcVVc3Bf8qvKhCPvW7";
+
+// Look for "Hello"
+var patt = /Hello/g;
+var result = patt.test(str);
+
+// Look for "W3Schools"
+patt2 = /W3Schools/g;
+result2 = patt2.test(str);
